d47e097c13b4ac1927e9129e990f4ceab4e09756
mkramers <mattkramers@gmail.com>
2019-04-29 00:30:57 -0400

    Improve physics sample

--- BepuSample/Program.cs
+++ BepuSample/Program.cs
@@ -3,6 +3,7 @@ using System.Numerics;
 using BepuPhysics;
 using BepuPhysics.Collidables;
 using BepuUtilities.Memory;
+using RenderCore;
 using RenderCore.Physics;
 
 namespace BepuSample
@@ -17,20 +18,8 @@ namespace BepuSample
             Simulation simulation = Simulation.Create(bufferPool, new NarrowPhaseCallbacks(),
                 new PoseIntegratorCallbacks(gravity));
 
-            //Drop a ball on a big static box.
-            Sphere sphere = new Sphere(1);
-            sphere.ComputeInertia(1, out BodyInertia sphereInertia);
-
-            Vector3 position = new Vector3(0, 5, 0);
-            TypedIndex sphereReference = simulation.Shapes.Add(sphere);
-            CollidableDescription collidableDescription = new CollidableDescription(sphereReference, 0.1f);
-            BodyActivityDescription bodyActivityDescription = new BodyActivityDescription(0.01f);
-
-            BodyDescription bodyDescription = BodyDescription.CreateDynamic(position, sphereInertia,
-                collidableDescription, bodyActivityDescription);
-
-            int bodyHandle = simulation.Bodies.Add(bodyDescription);
-
+            TestCharacter tc = new TestCharacter(simulation);
+            
             Vector3 staticPosition = new Vector3(0, 0, 0);
             Box staticBox = new Box(500, 1, 500);
             TypedIndex boxShapeReference = simulation.Shapes.Add(staticBox);
@@ -43,8 +32,8 @@ namespace BepuSample
             {
                 simulation.Timestep(0.01f);
 
-                simulation.Bodies.GetDescription(bodyHandle, out BodyDescription foundBody);
-                Console.WriteLine(foundBody.Pose.Position.ToString());
+                Vector3 bd = tc.GetPosition();
+                Console.WriteLine(bd.ToString());
             }
 
             simulation.Dispose();
---
+++ RenderCore/ICharacter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Numerics;
+using BepuPhysics;
+using BepuPhysics.Collidables;
+
+namespace RenderCore
+{
+    public interface ICharacter
+    {
+        BodyDescription GetBodyDescription();
+    }
+
+    public class TestCharacter : IDisposable
+    {
+        private readonly int m_bodyIndex;
+        private readonly TypedIndex m_shapeIndex;
+        private readonly Simulation m_simulation;
+
+        public TestCharacter(Simulation _simulation)
+        {
+            m_simulation = _simulation;
+
+            Sphere sphere = new Sphere(1);
+            sphere.ComputeInertia(1, out BodyInertia sphereInertia);
+
+            Vector3 position = new Vector3(0, 5, 0);
+
+            m_shapeIndex = _simulation.Shapes.Add(sphere);
+
+            CollidableDescription collidableDescription = new CollidableDescription(m_shapeIndex, 0.1f);
+            BodyActivityDescription bodyActivityDescription = new BodyActivityDescription(0.01f);
+
+            BodyDescription bodyDescription = BodyDescription.CreateDynamic(position, sphereInertia,
+                collidableDescription, bodyActivityDescription);
+
+            m_bodyIndex = _simulation.Bodies.Add(bodyDescription);
+        }
+
+        private BodyDescription GetBodyDescription()
+        {
+            m_simulation.Bodies.GetDescription(m_bodyIndex, out BodyDescription bodyDescription);
+
+            return bodyDescription;
+        }
+
+        public Vector3 GetPosition()
+        {
+            BodyDescription bodyDescription = GetBodyDescription();
+            return bodyDescription.Pose.Position;
+        }
+
+        private void RemoveFromSimulation()
+        {
+            m_simulation.Shapes.Remove(m_shapeIndex);
+            m_simulation.Bodies.Remove(m_bodyIndex);
+        }
+
+        public void Dispose()
+        {
+            RemoveFromSimulation();
+        }
+    }
+}
