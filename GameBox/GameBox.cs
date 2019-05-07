3d7e3cecccfb506c6b05ddc8282123573c3bf543
mkramers <mattkramers@gmail.com>
2019-05-06 23:52:21 -0400

    Extract landscape factory

--- GameBox/GameBox.cs
+++ GameBox/GameBox.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Numerics;
 using Aether.Physics2D.Dynamics;
 using RenderCore;
@@ -19,6 +18,7 @@ namespace GameBox
             CreateEntities();
         }
 
+
         private void CreateEntities()
         {
             IPhysics physics = GetPhysics();
@@ -29,7 +29,7 @@ namespace GameBox
 
             AddEntity(m_manEntity);
 
-            IEnumerable<IEntity> woodEntities = CreateLandscape();
+            IEnumerable<IEntity> woodEntities = LandscapeFactory.CreateLandscape(physics);
 
             foreach (IEntity woodEntity in woodEntities)
             {
@@ -46,64 +46,6 @@ namespace GameBox
 
             RenderCoreWindow renderCoreWindow = GetRenderCoreWindow();
             renderCoreWindow.SetViewController(m_viewController);
-        }
-
-        private IEnumerable<IEntity> CreateLandscape()
-        {
-            const int range = 20;
-
-            IPhysics physics = GetPhysics();
-
-            Vector2 position = new Vector2(-10, 5);
-            List<Vector2> positions = GetPyramid(position, range).ToList();
-
-            uint boxSize = (uint) Math.Round(1.5f * range);
-            IEnumerable<Vector2> box = GetBox(position, new Vector2u(boxSize, boxSize), 1);
-
-            positions.AddRange(box);
-
-            return positions.Select(_pyramidPosition =>
-                EntityFactory.CreateEntity(1, _pyramidPosition, physics, ResourceId.WOOD, BodyType.Static)).ToList();
-        }
-
-        private static IEnumerable<Vector2> GetPyramid(Vector2 _position, int _size)
-        {
-            List<Vector2> positions = new List<Vector2>();
-
-            for (int i = 0; i < _size; i++)
-            {
-                int height = i + 1;
-                for (int j = 0; j < height; j++)
-                {
-                    Vector2 position = _position + new Vector2(i, -j);
-                    positions.Add(position);
-                }
-            }
-
-            return positions;
-        }
-
-        private static IEnumerable<Vector2> GetBox(Vector2 _position, Vector2u _size, float _stepSize)
-        {
-            List<Vector2> positions = new List<Vector2>();
-
-            for (uint i = 0; i < _size.X; i++)
-            {
-                for (uint j = 0; j < _size.Y; j++)
-                {
-                    bool isOutsideRow = i == 0 || i == _size.X - 1;
-                    bool isOutsideColumn = j == 0 || j == _size.Y - 1;
-                    if (!isOutsideRow && !isOutsideColumn)
-                    {
-                        continue;
-                    }
-
-                    Vector2 position = _position + _stepSize * new Vector2(i, -j);
-                    positions.Add(position);
-                }
-            }
-
-            return positions;
         }
 
         public override void Tick(TimeSpan _elapsed)
---
+++ RenderCore/LandscapeFactory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Aether.Physics2D.Dynamics;
+using SFML.System;
+
+namespace RenderCore
+{
+    public class LandscapeFactory
+    {
+        public static IEnumerable<IEntity> CreateLandscape(IPhysics _physics)
+        {
+            const int range = 20;
+            
+            Vector2 position = new Vector2(-10, 5);
+            List<Vector2> positions = GetPyramid(position, range).ToList();
+
+            uint boxSize = (uint) Math.Round(1.5f * range);
+            IEnumerable<Vector2> box = GetBox(position, new Vector2u(boxSize, boxSize), 1);
+
+            positions.AddRange(box);
+
+            return positions.Select(_pyramidPosition =>
+                EntityFactory.CreateEntity(1, _pyramidPosition, _physics, ResourceId.WOOD, BodyType.Static)).ToList();
+        }
+
+        public static IEnumerable<Vector2> GetPyramid(Vector2 _position, int _size)
+        {
+            List<Vector2> positions = new List<Vector2>();
+
+            for (int i = 0; i < _size; i++)
+            {
+                int height = i + 1;
+                for (int j = 0; j < height; j++)
+                {
+                    Vector2 position = _position + new Vector2(i, -j);
+                    positions.Add(position);
+                }
+            }
+
+            return positions;
+        }
+
+        public static IEnumerable<Vector2> GetBox(Vector2 _position, Vector2u _size, float _stepSize)
+        {
+            List<Vector2> positions = new List<Vector2>();
+
+            for (uint i = 0; i < _size.X; i++)
+            {
+                for (uint j = 0; j < _size.Y; j++)
+                {
+                    bool isOutsideRow = i == 0 || i == _size.X - 1;
+                    bool isOutsideColumn = j == 0 || j == _size.Y - 1;
+                    if (!isOutsideRow && !isOutsideColumn)
+                    {
+                        continue;
+                    }
+
+                    Vector2 position = _position + _stepSize * new Vector2(i, -j);
+                    positions.Add(position);
+                }
+            }
+
+            return positions;
+        }
+    }
+}
