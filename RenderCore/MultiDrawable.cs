2119c0d617fa4124bc15486ac29900a40a4b577e
mkramers <mattkramers@gmail.com>
2019-05-21 08:44:53 -0400

    Simplify multi drawable

--- RenderCore/MultiDrawable.cs
+++ RenderCore/MultiDrawable.cs
@@ -5,24 +5,21 @@ using SFML.Graphics;
 
 namespace RenderCore
 {
-    public class MultiDrawable<T> : IPositionDrawable where T : Transformable, Drawable
+    public class MultiDrawable<T> : List<T>, IPositionDrawable where T : Transformable, Drawable
     {
         private Vector2 m_position;
-        private readonly List<T> m_renderObjects;
 
-        public MultiDrawable(List<T> _renderObjects)
+        public MultiDrawable(IEnumerable<T> _renderObjects) : base(_renderObjects)
         {
-            m_renderObjects = _renderObjects;
         }
 
         protected MultiDrawable()
         {
-            m_renderObjects = new List<T>();
         }
 
         public void Draw(RenderTarget _target, RenderStates _states)
         {
-            foreach (T drawable in m_renderObjects)
+            foreach (T drawable in this)
             {
                 drawable.Draw(_target, _states);
             }
@@ -33,16 +30,11 @@ namespace RenderCore
             return m_position;
         }
 
-        protected void AddRange(IEnumerable<T> _drawables)
-        {
-            m_renderObjects.AddRange(_drawables);
-        }
-
         public void SetPosition(Vector2 _position)
         {
             Vector2 offset = m_position - _position;
 
-            foreach (T drawable in m_renderObjects)
+            foreach (T drawable in this)
             {
                 Vector2 position = drawable.Position.GetVector2() + offset;
                 drawable.Position = position.GetVector2F();
@@ -58,12 +50,12 @@ namespace RenderCore
 
         protected void DisposeItemsAndClear()
         {
-            foreach (T drawable in m_renderObjects)
+            foreach (T drawable in this)
             {
                 drawable.Dispose();
             }
 
-            m_renderObjects.Clear();
+            Clear();
         }
     }
 }
