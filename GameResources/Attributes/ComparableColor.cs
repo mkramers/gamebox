20cb6887f82ccb3a7b933f6b5c16c94a889dc1e3
mkramers <mattkramers@gmail.com>
2019-07-10 22:55:27 -0400

    Add color constructor for convenience

--- GameResources/Attributes/ComparableColor.cs
+++ GameResources/Attributes/ComparableColor.cs
@@ -12,6 +12,11 @@ namespace GameResources.Attributes
             Color = _color;
         }
 
+        public ComparableColor(int _r, int _g, int _b, int _a)
+        {
+            Color = Color.FromArgb(_r, _g, _b, _a);
+        }
+
         private Color Color { get; }
 
         public int CompareTo(object _other)
