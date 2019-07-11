ada3fba5bd3bd230a1530d2171418b128b59108a
mkramers <mattkramers@gmail.com>
2019-07-10 23:48:26 -0400

    Split out head tail generator and began work on new generator

--- MarchingSquares/MarchingSquaresPolygonGenerator.cs
+++ MarchingSquares/MarchingSquaresPolygonGenerator.cs
@@ -81,38 +81,31 @@ namespace MarchingSquares
     {
         public IEnumerable<IVertexObject> GetVertexObjects(IEnumerable<LineSegment> _lineSegments)
         {
-            LineSegment[] lineSegments = _lineSegments as LineSegment[] ?? _lineSegments.ToArray();
+            List<LineSegment> lineSegments = _lineSegments.ToList();
 
             List<Polygon> vertexObjects = new List<Polygon>();
+
+            Polygon polygon = GetVertexObject(lineSegments);
+            vertexObjects.Add(polygon);
+
+            return vertexObjects;
+        }
+
+        public Polygon GetVertexObject(IEnumerable<LineSegment> _lineSegments)
+        {
+            Polygon polygon = new Polygon();
+
+            IEnumerable<LineSegment> lineSegments = _lineSegments as LineSegment[] ?? _lineSegments.ToArray();
             foreach (LineSegment lineSegment in lineSegments)
             {
-                foreach (LineSegment otherLineSegment in lineSegments)
+                LineSegment nextSegment =
+                    lineSegments.FirstOrDefault(_lineSegment => lineSegment.IsConnectedEndToStart(_lineSegment));
+                if (nextSegment != null)
                 {
-                    if (!lineSegment.IsConnectedEndToStart(otherLineSegment))
-                    {
-                        continue;
-                    }
-
-                    Polygon existingVertexObject = vertexObjects.FirstOrDefault(_vertexObject => _vertexObject.ContainsLineSegment(lineSegment));
-                    if (existingVertexObject != null)
-                    {
-                        if (!existingVertexObject.Contains(otherLineSegment.End))
-                        {
-                            existingVertexObject.Add(otherLineSegment.End);
-                        }
-                    }
-                    else
-                    {
-                        Polygon polygon = new Polygon();
-                        polygon.AddRange(lineSegment.ToArray());
-                        polygon.Add(otherLineSegment.End);
-
-                        vertexObjects.Add(polygon);
-                    }
                 }
             }
 
-            return vertexObjects;
+            return polygon;
         }
     }
 
