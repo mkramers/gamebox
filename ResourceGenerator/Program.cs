6863214827009252d011e4a8eae95cf1b99e1412
mkramers <mattkramers@gmail.com>
2019-07-31 08:50:32 -0400

    Add resource generator script and add new resource folder

---
+++ ResourceGenerator/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace ResourceGenerator
+{
+    internal static class Program
+    {
+        private static int Main(string[] _args)
+        {
+            const string resourceDirectory = @"C:\dev\GameBox\Resources";
+
+            string[] asepriteFiles = Directory.GetFiles(resourceDirectory, "*.aseprite", SearchOption.AllDirectories);
+            foreach (string asepriteFile in asepriteFiles)
+            {
+                int exitCode = AsepriteExporter.Export(asepriteFile);
+                if (exitCode != 0)
+                {
+                    return exitCode;
+                }
+            }
+
+            return 0;
+        }
+    }
+
+    public static class AsepriteExporter
+    {
+        public static int Export(string _asepriteFilePath)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(_asepriteFilePath);
+            string workingDirectory = Path.GetDirectoryName(_asepriteFilePath);
+            string outputDirectory = Path.Combine(workingDirectory, "gen");
+
+            Directory.CreateDirectory(outputDirectory);
+
+            string outputPngFormat = Path.Combine(outputDirectory, $"{fileName}-{{layer}}.png");
+            string outputJsonFormat = Path.Combine(outputDirectory, $"{fileName}.json");
+
+            string arguments =
+                $@"aseprite -b {fileName}.aseprite --save-as {outputPngFormat} --data {outputJsonFormat} --list-layers --format json-array";
+
+            ProcessStartInfo processStartInfo = new ProcessStartInfo
+            {
+                FileName = "aseprite",
+                Arguments = arguments,
+                WorkingDirectory = workingDirectory,
+            };
+
+            Process process = Process.Start(processStartInfo);
+            Debug.Assert(process != null);
+
+            process.WaitForExit();
+
+            return process.ExitCode;
+        }
+    }
+}
---
+++ Resources/palettes/generate.bat
@@ -0,0 +1,28 @@
+@echo off
+
+call :writeFile grey16.gpl 16
+call :writeFile grey8.gpl 8
+call :writeFile grey4.gpl 4
+
+:goto eof
+
+:writeFile
+
+SETLOCAL EnableDelayedExpansion
+
+set outputFile=%1
+set levels=%2
+
+echo GIMP Palette > %outputFile%
+echo. >> %outputFile%
+
+set /a max=255
+set /a step=(%max% + 1) / %levels%
+
+FOR /L %%i IN (%max%,-%step%,0) DO (
+	ECHO %%i %%i %%i Untitled >> %outputFile%
+)
+
+ENDLOCAL
+
+goto :eof
