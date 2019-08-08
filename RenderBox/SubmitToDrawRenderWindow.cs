a8ffe8026c50955516b79cc6ef3940a436c21fa6
mkramers <mattkramers@gmail.com>
2019-08-08 10:01:30 -0400

    Encapsulate window resizing

--- RenderBox/SubmitToDrawRenderWindow.cs
+++ RenderBox/SubmitToDrawRenderWindow.cs
@@ -129,6 +129,37 @@ namespace RenderBox
         public TimeSpan Elapsed { get; }
     }
 
+    public static class WindowResizer
+    {
+        public static FloatRect Resize(Vector2u _windowSize, float _aspectRatio)
+        {
+            float windowAspectRatio = (float) _windowSize.X / _windowSize.Y;
+            if (windowAspectRatio <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(windowAspectRatio), "negative aspect ratio not supported");
+            }
+
+            FloatRect viewPort = new FloatRect(0, 0, 1, 1);
+
+            if (windowAspectRatio > _aspectRatio)
+            {
+                float xPadding = (windowAspectRatio - _aspectRatio) / 2.0f;
+                viewPort = new FloatRect(xPadding / 2.0f, 0, 1 - xPadding, 1);
+            }
+            else if (windowAspectRatio < _aspectRatio)
+            {
+                float yPadding = (_aspectRatio - windowAspectRatio) / 2.0f;
+                viewPort = new FloatRect(0, yPadding / 2.0f, 0, 1 - yPadding);
+            }
+            else if (Math.Abs(windowAspectRatio - _aspectRatio) < 0.0001f)
+            {
+                viewPort = new FloatRect(0, 0, 1, 1);
+            }
+
+            return viewPort;
+        }
+    }
+
     public class SubmitToDrawRenderWindow : ITickable
     {
         private readonly float m_aspectRatio;
@@ -140,7 +171,7 @@ namespace RenderBox
         {
             m_aspectRatio = _aspectRatio;
             m_renderWindow = RenderWindowFactory.CreateRenderWindow("", _windowSize);
-            m_renderWindow.Resized += OnRenderWindowResized;
+            m_renderWindow.Resized += (_sender, _e) => Resize(new Vector2u(_e.Width, _e.Height));
 
             m_scene = new Scene();
 
@@ -160,39 +191,11 @@ namespace RenderBox
             return guiManager;
         }
 
-        private void OnRenderWindowResized(object _sender, SizeEventArgs _e)
-        {
-            uint width = _e.Width;
-            uint height = _e.Height;
-
-            Resize(new Vector2u(width, height));
-        }
-
         private void Resize(Vector2u _windowSize)
         {
-            float windowAspectRatio = (float)_windowSize.X / _windowSize.Y;
+            float aspectRatio = m_aspectRatio;
 
-            if (windowAspectRatio <= 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(windowAspectRatio), "negative aspect ratio not supported");
-            }
-
-            FloatRect viewPort = new FloatRect(0, 0, 1, 1);
-
-            if (windowAspectRatio > m_aspectRatio)
-            {
-                float xPadding = (windowAspectRatio - m_aspectRatio) / 2.0f;
-                viewPort = new FloatRect(xPadding / 2.0f, 0, 1 - xPadding, 1);
-            }
-            else if (windowAspectRatio < m_aspectRatio)
-            {
-                float yPadding = (m_aspectRatio - windowAspectRatio) / 2.0f;
-                viewPort = new FloatRect(0, yPadding / 2.0f, 0, 1 - yPadding);
-            }
-            else if (Math.Abs(windowAspectRatio - m_aspectRatio) < 0.0001f)
-            {
-                viewPort = new FloatRect(0, 0, 1, 1);
-            }
+            FloatRect viewPort = WindowResizer.Resize(_windowSize, aspectRatio);
 
             View renderWindowView = m_renderWindow.GetView();
             renderWindowView.Viewport = viewPort;
