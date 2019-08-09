e332319d06afb39978611c30a516b120ee15f467
mkramers <mattkramers@gmail.com>
2019-08-08 23:25:19 -0400

    Port test data to be platform independent

--- IOUtilities.Tests/PathUtilitiesTests.cs
+++ IOUtilities.Tests/PathUtilitiesTests.cs
@@ -9,8 +9,8 @@ namespace IOUtilities.Tests
         [Test]
         public void TestCanCalculateRelativePath()
         {
-            const string rootPath = "c:\\windows";
-            const string fullPath = "c:\\windows\\system32\\wininet.dll";
+            const string rootPath = ".\\windows";
+            const string fullPath = ".\\windows\\system32\\wininet.dll";
             const string expectedResult = ".\\system32\\wininet.dll";
 
             string result = PathUtilities.GetRelativePath(rootPath, fullPath);
@@ -21,8 +21,8 @@ namespace IOUtilities.Tests
         [Test]
         public void TestDifferencesDueToBackstepsDoesNotMatter()
         {
-            string format1 = PathUtilities.NormalizeFilepath("c:\\windows\\system32");
-            string format2 = PathUtilities.NormalizeFilepath("c:\\Program Files\\..\\Windows\\System32");
+            string format1 = PathUtilities.NormalizeFilepath(".\\windows\\system32");
+            string format2 = PathUtilities.NormalizeFilepath(".\\Program Files\\..\\Windows\\System32");
 
             Assert.AreEqual(format1, format2);
         }
@@ -30,8 +30,8 @@ namespace IOUtilities.Tests
         [Test]
         public void TestDifferencesInCapitalizationDoesNotMatter()
         {
-            string format1 = PathUtilities.NormalizeFilepath("c:\\windows\\system32");
-            string format2 = PathUtilities.NormalizeFilepath("c:\\WindowS\\System32");
+            string format1 = PathUtilities.NormalizeFilepath(".\\windows\\system32");
+            string format2 = PathUtilities.NormalizeFilepath(".\\WindowS\\System32");
 
             Assert.AreEqual(format1, format2);
         }
@@ -39,8 +39,8 @@ namespace IOUtilities.Tests
         [Test]
         public void TestDifferencesInFinalSlashDoesNotMatter()
         {
-            string format1 = PathUtilities.NormalizeFilepath("c:\\windows\\system32");
-            string format2 = PathUtilities.NormalizeFilepath("c:\\windows\\system32\\");
+            string format1 = PathUtilities.NormalizeFilepath(".\\windows\\system32");
+            string format2 = PathUtilities.NormalizeFilepath(".\\windows\\system32\\");
 
             Console.WriteLine(format1);
             Console.WriteLine(format2);
@@ -51,8 +51,8 @@ namespace IOUtilities.Tests
         [Test]
         public void TestThrowsExceptionIfRootDoesNotMatchFullPath()
         {
-            const string rootPath = "c:\\windows";
-            const string fullPath = "c:\\program files\\Internet Explorer\\iexplore.exe";
+            const string rootPath = ".\\windows";
+            const string fullPath = ".\\program files\\Internet Explorer\\iexplore.exe";
 
             try
             {
