4d7dfdc7be0fa2f6423481d40b0d25f041b93073
mkramers <mattkramers@gmail.com>
2019-08-13 23:38:43 -0400

    Add reflection utils

---
+++ Common/ReflectionUtilities/ReflectionUtilities.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Common.ReflectionUtilities
+{
+    /// <summary>
+    /// derived from https://stackoverflow.com/a/2362756/1620721
+    /// </summary>
+    public static class ReflectionUtilities
+    {
+        public static List<Type> FindAllDerivedTypes<T>()
+        {
+            return FindAllDerivedTypes<T>(Assembly.GetAssembly(typeof(T)));
+        }
+
+        private static List<Type> FindAllDerivedTypes<T>(Assembly _assembly)
+        {
+            Type derivedType = typeof(T);
+            return _assembly
+                .GetTypes()
+                .Where(_type =>
+                    _type != derivedType &&
+                    derivedType.IsAssignableFrom(_type)
+                ).ToList();
+        }
+    }
+}
