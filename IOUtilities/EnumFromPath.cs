8705156da0923b821e6556efb747ec68382eb469
mkramers <mattkramers@gmail.com>
2019-08-18 20:59:26 -0400

    Fix broken build

--- IOUtilities/EnumFromPath.cs
+++ IOUtilities/EnumFromPath.cs
@@ -9,17 +9,23 @@ namespace IOUtilities
     {
         public static T GetEnumFromPath<T>(string _filePath) where T : Enum
         {
+            string enumName = GetEnumFromPath(_filePath);
+
+            T enumValue = (T)Enum.Parse(typeof(T), enumName);
+            return enumValue;
+        }
+
+        public static string GetEnumFromPath(string _filePath)
+        {
             string filePathNoExtension = Path.ChangeExtension(_filePath, "")?.TrimEnd('.');
 
             List<string> names = filePathNoExtension.TrimStart('.')
-                .Split(new[] { "\\", "-" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                .Split(new[] {"\\", "-"}, StringSplitOptions.RemoveEmptyEntries).ToList();
 
             names.RemoveAt(names.Count - 2);
 
             string enumName = string.Join("_", names.Select(_name => _name.ToUpper()));
-
-            T enumValue = (T)Enum.Parse(typeof(T), enumName);
-            return enumValue;
+            return enumName;
         }
     }
 }
