6ff0e6ab1f31c3582d1a5c17b38768aa2477b0b2
mkramers <mattkramers@gmail.com>
2019-08-20 23:50:49 -0400

    Update path extensions to use ipath and enable test on travis

--- IOUtilities.Tests/PathExtensionsTests.cs
+++ IOUtilities.Tests/PathExtensionsTests.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Diagnostics;
+using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
 using NUnit.Framework;
 
 namespace IOUtilities.Tests
 {
     [TestFixture]
-    public class PathUtilitiesTests
+    public class PathExtensionsTests
     {
+        private static readonly IPath m_path = new MockFileSystem().Path;
+
         [Test]
         public void TestCanCalculateRelativePath()
         {
@@ -14,7 +18,7 @@ namespace IOUtilities.Tests
             const string fullPath = ".\\windows\\system32\\wininet.dll";
             const string expectedResult = ".\\system32\\wininet.dll";
 
-            string result = PathUtilities.GetRelativePath(rootPath, fullPath);
+            string result = m_path.GetRelativePath(rootPath, fullPath);
 
             Assert.AreEqual(expectedResult, result);
         }
@@ -22,8 +26,8 @@ namespace IOUtilities.Tests
         [Test]
         public void TestDifferencesDueToBackstepsDoesNotMatter()
         {
-            string format1 = PathUtilities.NormalizeFilepath(".\\windows\\system32");
-            string format2 = PathUtilities.NormalizeFilepath(".\\Program Files\\..\\Windows\\System32");
+            string format1 = m_path.NormalizeFilepath(".\\windows\\system32");
+            string format2 = m_path.NormalizeFilepath(".\\Program Files\\..\\Windows\\System32");
 
             Assert.AreEqual(format1, format2);
         }
@@ -31,8 +35,8 @@ namespace IOUtilities.Tests
         [Test]
         public void TestDifferencesInCapitalizationDoesNotMatter()
         {
-            string format1 = PathUtilities.NormalizeFilepath(".\\windows\\system32");
-            string format2 = PathUtilities.NormalizeFilepath(".\\WindowS\\System32");
+            string format1 = m_path.NormalizeFilepath(".\\windows\\system32");
+            string format2 = m_path.NormalizeFilepath(".\\WindowS\\System32");
 
             Assert.AreEqual(format1, format2);
         }
@@ -40,8 +44,8 @@ namespace IOUtilities.Tests
         [Test]
         public void TestDifferencesInFinalSlashDoesNotMatter()
         {
-            string format1 = PathUtilities.NormalizeFilepath(".\\windows\\system32");
-            string format2 = PathUtilities.NormalizeFilepath(".\\windows\\system32\\");
+            string format1 = m_path.NormalizeFilepath(".\\windows\\system32");
+            string format2 = m_path.NormalizeFilepath(".\\windows\\system32\\");
 
             Assert.AreEqual(format1, format2);
         }
@@ -52,7 +56,7 @@ namespace IOUtilities.Tests
             const string rootPath = ".\\windows";
             const string fullPath = ".\\program files\\Internet Explorer\\iexplore.exe";
 
-            Assert.Throws<Exception>(() => PathUtilities.GetRelativePath(rootPath, fullPath));
+            Assert.Throws<Exception>(() => m_path.GetRelativePath(rootPath, fullPath));
         }
     }
 }
--- IOUtilities/PathExtensions.cs
+++ IOUtilities/PathExtensions.cs
@@ -1,23 +1,24 @@
 ﻿using System;
 using System.IO;
+using System.IO.Abstractions;
 
 namespace IOUtilities
 {
-    public static class PathUtilities
+    public static class PathExtensions
     {
-        public static string NormalizeFilepath(string _filepath)
+        public static string NormalizeFilepath(this IPath _path, string _filepath)
         {
-            string result = Path.GetFullPath(_filepath).ToLowerInvariant();
+            string result = _path.GetFullPath(_filepath).ToLowerInvariant();
 
             result = result.TrimEnd(Path.DirectorySeparatorChar);
 
             return result;
         }
 
-        public static string GetRelativePath(string _rootPath, string _fullPath)
+        public static string GetRelativePath(this IPath _path, string _rootPath, string _fullPath)
         {
-            _rootPath = NormalizeFilepath(_rootPath);
-            _fullPath = NormalizeFilepath(_fullPath);
+            _rootPath = _path.NormalizeFilepath(_rootPath);
+            _fullPath = _path.NormalizeFilepath(_fullPath);
 
             if (!_fullPath.StartsWith(_rootPath))
             {
