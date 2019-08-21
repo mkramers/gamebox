501313d6d35171193f8d5bf95fd39f3c564637a3
mkramers <mattkramers@gmail.com>
2019-08-20 23:20:15 -0400

    Add dispose extensions

---
+++ Common.Tests/Extensions/DisposableExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common.Extensions;
+using NUnit.Framework;
+
+namespace Common.Tests.Extensions
+{
+    [TestFixture]
+    public class DisposableExtensionsTests
+    {
+        private class TestDisposable : IDisposable
+        {
+            public bool IsDisposed { get; private set; }
+
+            public void Dispose()
+            {
+                IsDisposed = true;
+            }
+        }
+
+        [Test]
+        public void DisposesAllAndClearsCorrectly()
+        {
+            TestDisposable[] allDisposables = { new TestDisposable(), new TestDisposable() };
+
+            List<TestDisposable> disposables = new List<TestDisposable>(allDisposables);
+            disposables.DisposeAllAndClear();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(disposables.Count == 0);
+                Assert.That(allDisposables.All(_disposable => _disposable.IsDisposed), Is.True, "Not all items were disposed");
+            });
+        }
+    }
+}
---
+++ Common/Extensions/DisposableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Extensions
+{
+    public static class DisposableExtensions
+    {
+        public static void DisposeAllAndClear<T>(this IList<T> _items) where T : IDisposable
+        {
+            foreach (T disposable in _items)
+            {
+                disposable.Dispose();
+            }
+
+            _items.Clear();
+        }
+    }
+}
